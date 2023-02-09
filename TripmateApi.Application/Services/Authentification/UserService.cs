5700f73faf396f4400134fbdee746438f8d8a69d
Your Name <you@example.com>
2023-02-09 10:28:42 +0100

    feat : userrepository & méthode authenticate

--- TripmateApi.Application/Services/Authentification/UserService.cs
+++ TripmateApi.Application/Services/Authentification/UserService.cs
@@ -2,18 +2,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using TripmateApi.Application.Common.Models.Authentification;
 using TripmateApi.Application.Services.Authentification.Interfaces;
+using TripmateApi.Entities.Entities;
+using TripmateApi.Infrastructure.Repositories;
 
 namespace TripmateApi.Application.Services.Authentification
 {
     public class UserService : IUserService
     {
-        public Task<Result<LoginResponseDto>> Authenticate(LoginRequestDto model)
+        private readonly UserRepository _repo;
+
+        public UserService(UserRepository repo)
         {
-            throw new NotImplementedException();
+            _repo = repo;
+        }
+        private string generateHashedPassword(string password)
+        {
+            SHA256 sha256Hash = SHA256.Create();
+            byte[] data = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            var sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();         
+        }
+
+        public async Task<Result<LoginResponseDto>> Authenticate(LoginRequestDto model)
+        {
+            string hashedPassword = generateHashedPassword(model.Password);
+            User exist = await _repo.FindOne(user => user.Email == model.Email && user.Password == hashedPassword);
+            if (exist == null)
+                return Result.Failure<LoginResponseDto>("Aucun utilisateur n'a été trouvé avec cet email & ce mot de passe");
+            return Result.Success(new LoginResponseDto());
         }
     }
 }
